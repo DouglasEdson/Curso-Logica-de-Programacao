528bf10be96cc18430e42913db4c56dcc509e0e8
DouglasEdson <douglas22@live.com.pt>
2022-01-14 16:46:19 -0300

    Elaborando um algoritmo no Eclipse

--- src/Exercicio01.java
+++ src/Exercicio01.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class Exercicio01 {
 
 	public static void main(String[] args) {
-		System.out.println("Imprimindo uma mensagem diferente");
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite o valor do produto: ");
+		Double valorProduto = scanner.nextDouble();
+
+		System.out.print("Digite quantidade passada pelo cliente: ");
+		Double valorPassadoPeloCliente = scanner.nextDouble();
+
+		Double resultado = valorPassadoPeloCliente - valorProduto;
+
+		System.out.println("Troco: " + resultado);
+
+		scanner.close();
 
 	}
 
